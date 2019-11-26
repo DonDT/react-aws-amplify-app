48235c545141cf0f81745ad9e786919f409f1707
Donald <ediagee@yahoo.com>
2019-11-26 12:54:37 +0200

    more CRUD operations

--- src/App.js
+++ src/App.js
@@ -1,22 +1,99 @@
 import React, { Component } from "react";
 import { withAuthenticator } from "aws-amplify-react";
 import { API, graphqlOperation } from "aws-amplify";
-import { createNote } from "./graphql/mutations";
+import { createNote, deleteNote, updateNote } from "./graphql/mutations";
+import { listNotes } from "./graphql/queries";
+
 import "./app.css";
 
 class App extends Component {
   state = {
-    note: ""
+    note: "",
+    notes: [],
+    id: ""
   };
+
+  async componentDidMount() {
+    const result = await API.graphql(graphqlOperation(listNotes));
+    this.setState({
+      notes: result.data.listNotes.items
+    });
+  }
 
   handleInputChange = event =>
     this.setState({
       note: event.target.value
     });
-  handleSubmitForm = event => {
+
+  hasExistingData = () => {
+    const { notes, id } = this.state;
+    if (id) {
+      const itExist = notes.findIndex(note => note.id === id) > -1;
+
+      return itExist;
+    }
+    return false;
+  };
+
+  handleSubmitForm = async event => {
     event.preventDefault();
-    const { note } = this.state;
-    API.graphql(graphqlOperation(createNote, { input: { note } }));
+    const { note, notes } = this.state;
+
+    if (this.hasExistingData()) {
+      this.handleUpdate();
+    } else {
+      const result = await API.graphql(
+        graphqlOperation(createNote, { input: { note } })
+      );
+      const newNote = result.data.createNote;
+      const updatedNotes = [newNote, ...notes];
+      this.setState({
+        notes: updatedNotes,
+        note: ""
+      });
+    }
+  };
+
+  handleUpdate = async () => {
+    const { notes, id, note } = this.state;
+    const input = { id, note };
+    const result = await API.graphql(graphqlOperation(updateNote, { input }));
+
+    const updatedNote = result.data.updateNote;
+
+    const index = notes.findIndex(note => note.id === updatedNote.id);
+
+    const updatedNotes = [
+      ...notes.slice(0, index),
+      updatedNote,
+      ...notes.slice(index + 1)
+    ];
+    this.setState({
+      notes: updatedNotes,
+      note: "",
+      id: ""
+    });
+  };
+
+  handleDelete = async id => {
+    const { notes } = this.state;
+    const input = { id: id };
+
+    const result = await API.graphql(graphqlOperation(deleteNote, { input }));
+    const deletedItem = result.data.deleteNote.id;
+
+    const updatedNoteState = notes.filter(note => note.id !== deletedItem);
+
+    this.setState({
+      notes: updatedNoteState
+    });
+  };
+
+  handleSetNoteToInput = item => {
+    this.setState({
+      note: item.note,
+      id: item.id
+    });
   };
 
   render() {
@@ -30,20 +107,32 @@ class App extends Component {
             className="inputForm"
             type="text"
             placeholder="Write here"
+            value={this.state.note}
             onChange={this.handleInputChange}
           />
-          <button type="submit">Add Note</button>
+          <button type="submit">
+            {this.state.id ? "Update Note" : "Add Note"}
+          </button>
         </form>
-        {/* <div>
-          {notes.map(item => (
+        <div>
+          {this.state.notes.map(item => (
             <div key={item.id} className="notesDiv">
-              <li className="listItem">{item.note}</li>
-              <button className="listItemButton">
+              <li
+                className="listItem"
+                onClick={() => this.handleSetNoteToInput(item)}
+              >
+                {item.note}
+              </li>
+              <button
+                className="listItemButton
+              "
+                onClick={() => this.handleDelete(item.id)}
+              >
                 <span>&times;</span>
               </button>
             </div>
           ))}
-        </div> */}
+        </div>
       </div>
     );
   }
